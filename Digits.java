44e59612b0436e4123cf43047efe14dcd3d40c38
Aditya Gangil <111217346+adityagangil@users.noreply.github.com>
2023-03-18 09:21:04 +0530

    Add files via upload

---
+++ Digits.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class SumOfDigits {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = input.nextInt();
+        int sum = 0;
+
+        while (number != 0) {
+            int digit = number % 10;
+            sum += digit;
+            number /= 10;
+        }
+
+        System.out.println("Sum of digits: " + sum);
+    }
+}
